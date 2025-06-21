229aa7d607d47f64a18e89953c744cf168159088
JasirLimani <jasirlimani12@gmail.com>
2025-06-21 21:11:10 +0200

    insha allah i finished the project

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
 import Footer from "@/components/shared/Footer";
 import BackgroundBlob from "@/components/shared/BackgorundBlob";
+import ScrollToTop from "./ScrollToTop";
 
 export const metadata = {
     icons: {
@@ -104,6 +105,7 @@ export default function RootLayout({ children }) {
             <body>
                 <BackgroundBlob count={20} />
                 <Navbar />
+                <ScrollToTop />
                 {children}
                 <Footer />
             </body>
